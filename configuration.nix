f39ecb4f26839d1dce99a3f620f399da6cc44fbe
Robert Maurizio <app@maurizio.ooo>
2025-08-27 23:59:25 -0500

    fixed some service names

--- configuration.nix
+++ configuration.nix
@@ -20,10 +20,10 @@
   # Desktop Environment
   services.xserver = {
     enable = true;
-    displayManager.gdm.enable = true;
-    desktopManager.gnome.enable = true;
     xkb.layout = "us";
   };
+  services.displayManager.gdm.enable = true;
+  services.desktopManager.gnome.enable = true;
 
   # Audio
   services.pulseaudio.enable = false;
