799a1ece4b72d037675198af33482618d06e4505
Robert Maurizio <app@maurizio.ooo>
2025-08-28 14:48:05 -0500

    dns server change

--- configuration.nix
+++ configuration.nix
@@ -1,23 +1,18 @@
 { config, pkgs, ... }:
-
 {
   imports = [ ./hardware-configuration.nix ];
-
   # Boot
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-
   # Basic system config
   networking.hostName = "nixos";
   networking.networkmanager.enable = true;
   time.timeZone = "America/Chicago";
   i18n.defaultLocale = "en_US.UTF-8";
-
   # Desktop
   services.xserver.enable = true;
   services.displayManager.gdm.enable = true;
   services.desktopManager.gnome.enable = true;
-
   # Audio
   services.pulseaudio.enable = false;
   security.rtkit.enable = true;
@@ -27,28 +22,23 @@
     alsa.support32Bit = true;
     pulse.enable = true;
   };
-
   # Basic services
   services.flatpak.enable = true;
   services.printing.enable = true;
   services.openssh.enable = true;
-
   # Flatpak portals (better integration)
   xdg.portal.enable = true;
-
   # User
   users.users.rob = {
     isNormalUser = true;
     extraGroups = [ "networkmanager" "wheel" ];
     shell = pkgs.zsh;  # Set zsh as default shell
   };
-
   # Autologin
   services.displayManager.autoLogin = {
     enable = true;
     user = "rob";
   };
-
   # System-wide packages (CLI tools that root might need)
   environment.systemPackages = with pkgs; [
     bat
@@ -67,14 +57,11 @@
     xz
     zip
   ];
-
   # Enable shells system-wide
   programs.zsh.enable = true;  # Required for user shell
   programs.bash.completion.enable = true;
-
   # Set default shell for new users
   users.defaultUserShell = pkgs.zsh;
-
   # Fonts (system-wide)
   fonts.packages = with pkgs; [
     dejavu_fonts
@@ -88,29 +75,23 @@
     roboto-mono
     roboto-serif
   ];
-
   # Flathub remote
   systemd.services.flatpak-repo = {
     wantedBy = [ "multi-user.target" ];
     path = [ pkgs.flatpak ];
     script = "flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo";
   };
-
   # Enable flakes
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
-
   # Automatic garbage collection
   nix.gc = {
     automatic = true;
     dates = "weekly";
     options = "--delete-older-than 14d";
   };
-
   # Allow unfree globally
   nixpkgs.config.allowUnfree = true;
-
   # Enable locate database (useful for finding files)
   services.locate.enable = true;
-
   system.stateVersion = "25.05";
 }
